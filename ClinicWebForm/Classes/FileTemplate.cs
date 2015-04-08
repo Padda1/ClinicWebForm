b1477fa4cc0852de27092621485b952aec9ffaae
Padda1 <thepadda1@gmail.com>
2015-04-08 21:02:12 +0200

    Added FileTemplate

---
+++ ClinicWebForm/Classes/FileTemplate.cs
@@ -0,0 +1,139 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicWebForm.Classes
+{
+    [IgnoreEmptyLines()]
+    [DelimitedRecord(",")]
+    public class FileTemplate
+    {
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Municipality;
+	
+        [FieldTrim(TrimMode.Both)]
+        public String WardNo;
+
+        [FieldTrim(TrimMode.Both)]
+        public String ClinicName;
+
+        [FieldTrim(TrimMode.Both)]
+        public String OfficialHouseRegistrationNumber;
+
+        [FieldTrim(TrimMode.Both)]
+        public String HouseholdStreetAddressLocationRow1;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CHWHouseholdIdentifierNo;
+
+        [FieldTrim(TrimMode.Both)]
+        public String DateOfVisit;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CHWName;
+
+        [FieldTrim(TrimMode.Both)]
+        public String NameOfCaregiver;
+
+        [FieldTrim(TrimMode.Both)]
+        public String AgeOfCaregiver;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CareGiverContactNumber;
+
+        [FieldTrim(TrimMode.Both)]
+        public String EDD;
+
+        [FieldTrim(TrimMode.Both)]
+        public String NameOfChild;
+
+        [FieldTrim(TrimMode.Both)]
+        public String DateOfBirthOfChild;
+
+        [FieldTrim(TrimMode.Both)]
+        public String GenderOfChild;
+
+        [FieldTrim(TrimMode.Both)]
+        public String BirthWeight;
+
+        [FieldTrim(TrimMode.Both)]
+        public String ChildAge;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Grants;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Exposed;
+
+        [FieldTrim(TrimMode.Both)]
+        public String DateOfVisit1;
+
+        [FieldTrim(TrimMode.Both)]
+        public String DateOfFollowup;
+
+        [FieldTrim(TrimMode.Both)]
+        public String ANCPNCUnder5LBW;
+
+        [FieldTrim(TrimMode.Both)]
+        public String MaternalHealth;
+
+        [FieldTrim(TrimMode.Both)]
+        public String ChildHealth;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CareInterventionsProvided;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Referred;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CHWCaregiverSignature;
+
+        [FieldTrim(TrimMode.Both)]
+        public String CaregiverSignature;
+
+        [FieldTrim(TrimMode.Both)]
+        public String TeamLeaderConsultDate;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+
+        [FieldTrim(TrimMode.Both)]
+        public String Province;
+    }
+}
--- ClinicWebForm/Controllers/FileController.cs
+++ ClinicWebForm/Controllers/FileController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FileHelpers;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -27,9 +29,28 @@ namespace ClinicWebForm.Controllers
                 // store the file inside ~/App_Data/uploads folder
                 var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
                 file.SaveAs(path);
+
+                ImportFileIntoDatabase(path);
             }
             // redirect back to the index action to show the form once again
             return RedirectToAction("Index");
+        }
+
+        private DataTable LoadIntoCollection(string path)
+        {
+            //TODO: Update to use the actual file
+            FileHelperEngine engine = new FileHelperEngine(typeof(string));
+            return engine.ReadFileAsDT(path);
+        }
+
+        private void ImportFileIntoDatabase(string path)
+        {
+            DataTable data = LoadIntoCollection(path);
+
+            foreach(DataRow row in data.Rows)
+            {
+                //TODO: Split columns into objects
+            }
         }
     }
 }
